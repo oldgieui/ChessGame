4c9093ea27abb54bfe65ebd365e3c311ce17a86a
oldgieui <flecher1@gmail.com>
2013-07-12 15:43:35 +0900

    연습문제 1-5

--- testsrc/pawntest.java
+++ testsrc/pawntest.java
@@ -6,5 +6,10 @@ public class pawntest extends TestCase {
 		Pawn pawn1 = new Pawn("white");
 		String color = pawn1.whatColor();
 		assertEquals("white", color);
+		
+
+		Pawn pawn2 = new Pawn("black");
+		String color2 = pawn2.whatColor();
+		assertEquals("black", color2);
 	}
 }
