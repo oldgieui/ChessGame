cf3e0d123beaa4520a98c8c260d23702b25084ec
oldgieui <flecher1@gmail.com>
2013-07-17 18:35:30 +0900

    연습문제 2-1

--- testsrc/pawntest.java
+++ testsrc/pawntest.java
@@ -13,5 +13,10 @@ public class pawntest extends TestCase {
 		Pawn pawn2 = new Pawn(black);
 		String color2 = pawn2.whatColor();
 		assertEquals(black, color2);
+		
+//		이하 2장 과제
+		
+		Pawn pawn3 = new Pawn();
+		//인수가 필요한 생성자인데 인수를 사용하지 않았으므로 컴파일 에러가 생긴다.
 	}
 }
