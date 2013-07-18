9ba750f2f3b836580fa03ee50791a08b7f695134
oldgieui <flecher1@gmail.com>
2013-07-18 14:26:52 +0900

    연습문제 2-4

--- testsrc/Board.java
+++ testsrc/Board.java
@@ -1,5 +1,33 @@
-
+import java.util.ArrayList;
 
 public class Board {
-	public static int numberOfPawn = 0;
+	public static int numberOfWhitePawn = 0;
+	public static int numberOfBlackPawn = 0;
+	static ArrayList<Pawn> pawnList = new ArrayList<Pawn>();
+	
+	Board(){
+		
+	}
+	
+	static void addPawn(Pawn pawn) {
+		if (pawn.whatColor() == Pawn.white){
+			pawnList.add(pawn);
+			numberOfWhitePawn += 1;
+			System.out.println(pawnList);
+		}
+		
+		else if (pawn.whatColor() == Pawn.black){
+			pawnList.add(pawn);
+			numberOfBlackPawn += 1;
+			System.out.println(pawnList);
+		}
+		
+		else
+			System.out.println(pawnList);
+		
+	}
+	
+	static int numberOfPawn() {
+		return numberOfWhitePawn + numberOfBlackPawn;
+	}
 }
--- testsrc/BoardTest.java
+++ testsrc/BoardTest.java
@@ -3,6 +3,12 @@ import junit.framework.TestCase;
 
 public class BoardTest extends TestCase {
 	public void testcreate() throws Exception {
-		assertEquals(0, Board.numberOfPawn);
+		Pawn pawn1 = new Pawn(Pawn.white);
+		Pawn pawn2 = new Pawn(Pawn.black);
+		
+		assertEquals(0, Board.numberOfPawn());
+		Board.addPawn(pawn1);
+		Board.addPawn(pawn2);
+		
 	}
 }
